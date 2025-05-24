63f55dec9b4b719a78e9967c56bf9039a3c16adf
Hisham <hishammuhammed.in@gmail.com>
2025-05-24 17:21:41 +0530

    Add Home component with welcome message to index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Quick Flayer</h1>
+      <p>Your application is now running correctly!</p>
+    </div>
+  );
+}
